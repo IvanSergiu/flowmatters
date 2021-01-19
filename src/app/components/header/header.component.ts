8c7b204fb384bd5ac383c051a4195c9ef8cff907
IvanSergiu <ivansergiu6@gmail.com>
2021-01-19 22:32:48 +0200

    reponsive layout

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -10,14 +10,33 @@ export class HeaderComponent implements OnInit {
   constructor() {}
 
   ngOnInit(): void {
-    $(document).ready(function () {
-      var $header = $('header'),
-        $clone = $header.before($header.clone().addClass('clone'));
+    // $(document).ready(function () {
+    //   var $header = $('#header'),
+    //     $clone = $header.before($header.clone().addClass('clone'));
 
-      $(window).on('scroll', function () {
-        var fromTop = $(window).scrollTop();
-        $('body').toggleClass('down', fromTop > 200);
-      });
+    //   $(window).on('scroll', function () {
+    //     var fromTop = $(window).scrollTop();
+    //     $('body').toggleClass('down', fromTop > 200);
+    //   });
+    // });
+    $(window).scroll(function () {
+      if ( $(this).scrollTop() > 200 && !$('header').hasClass('fixed-header ') ) {
+        $('header').addClass('fixed-header ');
+        $('header').removeClass('display-block');
+        $('header').slideDown();
+       } else if ( $(this).scrollTop() <= 0 ) {
+        $('header').addClass('display-block');
+        $('header').removeClass('fixed-header ');
+      }
     });
+  }
+  openNav() {
+    document.getElementById("sidemenu").style.width = "250px";
+    document.getElementById("sidemenu-content").style.marginLeft = "250px";
+  }
+  
+  closeNav() {
+    document.getElementById("sidemenu").style.width = "0";
+    document.getElementById("sidemenu-content").style.marginLeft= "0";
   }
 }
